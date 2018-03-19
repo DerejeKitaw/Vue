a3e02a95a66be46ec79b17e05cc37cb2ee52cc56
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-19 17:06:17 -0400

    Create svg.vue

---
+++ 07_Scaling Vue with Single File Components/src/components/svg.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <svg width="100" height="100">
+      <circle :cx="cx" :cy="cy" r="40" :stroke="color" stroke-width="4" fill="yellow" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cx: "50",
+      cy: "50",
+      color:"red"
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
